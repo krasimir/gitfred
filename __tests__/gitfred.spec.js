034a8e63f81f198b4c446cad9f8ca9eabd43f8d0
Krasimir Tsonev <krasimir@outset.ws>
2018-12-29 16:32:36 +0200

    Test setup

---
+++ __tests__/gitfred.spec.js
@@ -0,0 +1,9 @@
+const gitfred = require('../gitfred');
+
+describe('Given the gitfred library', () => {
+  describe('when initializing the library', () => {
+    it('should create something', () => {
+      expect(1).toEqual(1);
+    });
+  });
+});
